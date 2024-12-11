92d8612bdb58ffd3474f609f13c022fc949864fa
Jai Sachdev <jsachdev@pppl.gov>
2024-12-11 08:11:52 -0500

    Clean up of quotes

--- bpcmp/bpcmp.py
+++ bpcmp/bpcmp.py
@@ -33,50 +33,50 @@ def main():
     """
 
     # Print welcome message
-    print(colored('bpcmp: ADIOS2 bp output comparison utility\n', attrs=['bold']))
+    print(colored("bpcmp: ADIOS2 bp output comparison utility\n", attrs=["bold"]))
 
     # Counter for the number of detected differences
     num_differences = 0
 
     # Argument parsing configuration
-    description = 'bpcmp utility for comparing ADIOS2 bp output'
+    description = "bpcmp utility for comparing ADIOS2 bp output"
     parser = argparse.ArgumentParser(description=description)
-    parser.add_argument('output1', help='Path to the first ADIOS2 bp output file')
-    parser.add_argument('output2', help='Path to the second ADIOS2 bp output file')
-    parser.add_argument('-r', '--rtol', help='Relative tolerance (default: 0.0)', type=float, default=0.0)
-    parser.add_argument('-a', '--atol', help='Absolute tolerance (default: 0.0)', type=float, default=0.0)
-    parser.add_argument('-v', '--verbose', help='Verbosity level: 0, 1, or 2 (default: 0)', type=int, default=0)
-    parser.add_argument('--ignore-atts', help='Attributes to ignore', nargs='+', default=None)
-    parser.add_argument('--ignore-vars', help='Variables to ignore', nargs='+', default=None)
+    parser.add_argument("output1", help="Path to the first ADIOS2 bp output file")
+    parser.add_argument("output2", help="Path to the second ADIOS2 bp output file")
+    parser.add_argument("-r", "--rtol", help="Relative tolerance (default: 0.0)", type=float, default=0.0)
+    parser.add_argument("-a", "--atol", help="Absolute tolerance (default: 0.0)", type=float, default=0.0)
+    parser.add_argument("-v", "--verbose", help="Verbosity level: 0, 1, or 2 (default: 0)", type=int, default=0)
+    parser.add_argument("--ignore-atts", help="Attributes to ignore", nargs="+", default=None)
+    parser.add_argument("--ignore-vars", help="Variables to ignore", nargs="+", default=None)
 
     # Parse command-line arguments
     args = parser.parse_args()
 
     # Validate the paths to the output files
     if not os.path.exists(args.output1):
-        msg = colored(f'ERROR: Output file does not exist: {args.output1}', color='red', attrs=['bold'])
+        msg = colored(f"ERROR: Output file does not exist: {args.output1}", color="red", attrs=["bold"])
         raise FileNotFoundError(msg)
     output1 = args.output1
 
     if not os.path.exists(args.output2):
-        msg = colored(f'ERROR: Output file does not exist: {args.output2}', color='red', attrs=['bold'])
+        msg = colored(f"ERROR: Output file does not exist: {args.output2}", color="red", attrs=["bold"])
         raise FileNotFoundError(msg)
     output2 = args.output2
 
     # Validate and set tolerances
     if args.atol < 0.0:
-        msg = colored(f'ERROR: Absolute tolerance cannot be negative: {args.atol}', color='red', attrs=['bold'])
+        msg = colored(f"ERROR: Absolute tolerance cannot be negative: {args.atol}", color="red", attrs=["bold"])
         raise argparse.ArgumentTypeError(msg)
     atol = args.atol
 
     if args.rtol < 0.0:
-        msg = colored(f'ERROR: Relative tolerance cannot be negative: {args.rtol}', color='red', attrs=['bold'])
+        msg = colored(f"ERROR: Relative tolerance cannot be negative: {args.rtol}", color="red", attrs=["bold"])
         raise argparse.ArgumentTypeError(msg)
     rtol = args.rtol
 
     # Set verbosity level
     if args.verbose not in (0, 1, 2):
-        msg = colored('Invalid verbosity level. Choose from: 0 (none), 1 (errors), 2 (all)', color='red', attrs=["bold"])
+        msg = colored("Invalid verbosity level. Choose from: 0 (none), 1 (errors), 2 (all)", color="red", attrs=["bold"])
         raise argparse.ArgumentTypeError(msg)
     verbose = args.verbose
 
@@ -85,16 +85,16 @@ def main():
     ignore_vars = args.ignore_vars.copy() if args.ignore_vars else []
 
     # Display a summary of the inputs
-    print(f'ADIOS2 bp output 1: {output1}')
-    print(f'ADIOS2 bp output 2: {output2}')
-    print(f'Absolute tolerance: {atol:e}')
-    print(f'Relative tolerance: {rtol:e}')
-    print(f'Verbose level: {verbose}')
+    print(f"ADIOS2 bp output 1: {output1}")
+    print(f"ADIOS2 bp output 2: {output2}")
+    print(f"Absolute tolerance: {atol:e}")
+    print(f"Relative tolerance: {rtol:e}")
+    print(f"Verbose level: {verbose}")
     if ignore_atts:
-        print(f'Attributes to ignore: {ignore_atts}')
+        print(f"Attributes to ignore: {ignore_atts}")
     if ignore_vars:
-        print(f'Variables to ignore: {ignore_vars}')
-    print('')
+        print(f"Variables to ignore: {ignore_vars}")
+    print("")
 
     def compare_values(v1, v2):
         """
@@ -128,36 +128,36 @@ def main():
             continue
 
         # Read attribute from output 1
-        v1 = f1.available_attributes()[v]['Value']
+        v1 = f1.available_attributes()[v]["Value"]
 
         # Read attribute from output 2, report as difference if attribute does not exist
         try:
-            v2 = f2.available_attributes()[v]['Value']
+            v2 = f2.available_attributes()[v]["Value"]
         except Exception:
             num_differences += 1
             if verbose:
-                att = colored(f'{v}', color='yellow', attrs=['bold'])
-                msg = (colored('NOATT: ', color='yellow', attrs=['bold']) +
-                       f'Attribute {att} found in {output1} but not {output2}')
+                att = colored(f"{v}", color="yellow", attrs=["bold"])
+                msg = (colored("NOATT: ", color="yellow", attrs=["bold"]) +
+                       f"Attribute {att} found in {output1} but not {output2}")
                 print(msg)
             continue
 
         # Compare the attributes based on types
-        if f1.available_attributes()[v]['Type'] == 'string':
+        if f1.available_attributes()[v]["Type"] == "string":
             if v1 != v2:
                 num_differences += 1
                 if verbose:
-                    att = colored(f'{v}', color='red', attrs=['bold'])
-                    msg = (colored('DIFF:  ', color='red', attrs=['bold']) +
-                           f'Attribute {att} has differences: ' +
-                           f'{output1} = {v1} and ' +
-                           f'{output2} = {v2}')
+                    att = colored(f"{v}", color="red", attrs=["bold"])
+                    msg = (colored("DIFF:  ", color="red", attrs=["bold"]) +
+                           f"Attribute {att} has differences: " +
+                           f"{output1} = {v1} and " +
+                           f"{output2} = {v2}")
                     print(msg)
             else:
                 if verbose == 2:
-                    att = colored(f'{v}', color='blue', attrs=['bold'])
-                    msg = (colored('PASS:  ', color='blue', attrs=['bold']) +
-                           f'Attribute {att} is the same in both outputs')
+                    att = colored(f"{v}", color="blue", attrs=["bold"])
+                    msg = (colored("PASS:  ", color="blue", attrs=["bold"]) +
+                           f"Attribute {att} is the same in both outputs")
                     print(msg)
         else:
             # Re-reading input as integer or float (could be scalars or lists)
@@ -167,26 +167,26 @@ def main():
             if maxdiff is None:
                 num_differences += 1
                 if verbose:
-                    att = colored(f'{v}', color='red', attrs=['bold'])
-                    msg = (colored('ERROR: ', color='red', attrs=['bold']) +
-                           f'Attribute {att} has inconsistent types: ' +
-                           f'{output1} = {v1} and ' +
-                           f'{output2} = {v2}')
+                    att = colored(f"{v}", color="red", attrs=["bold"])
+                    msg = (colored("ERROR: ", color="red", attrs=["bold"]) +
+                           f"Attribute {att} has inconsistent types: " +
+                           f"{output1} = {v1} and " +
+                           f"{output2} = {v2}")
                     print(msg)
             elif not same:
                 num_differences += 1
                 if verbose:
-                    att = colored(f'{v}', color='red', attrs=['bold'])
-                    msg = (colored('DIFF:  ', color='red', attrs=['bold']) +
-                           f'Attribute {att} has differences, max difference: ' +
-                           f'{maxdiff}')
+                    att = colored(f"{v}", color="red", attrs=["bold"])
+                    msg = (colored("DIFF:  ", color="red", attrs=["bold"]) +
+                           f"Attribute {att} has differences, max difference: " +
+                           f"{maxdiff}")
                     print(msg)
             else:
                 if verbose == 2:
-                    msg = ''
-                    att = colored(f'{v}', color='blue', attrs=['bold'])
-                    msg = (colored('PASS:  ', color='blue', attrs=['bold']) +
-                           f'Attribute {att} is the same in both outputs')
+                    msg = ""
+                    att = colored(f"{v}", color="blue", attrs=["bold"])
+                    msg = (colored("PASS:  ", color="blue", attrs=["bold"]) +
+                           f"Attribute {att} is the same in both outputs")
                     print(msg)
 
     # Compare variables (note that in ADIOS 2, variables cannot be strings)
@@ -204,9 +204,9 @@ def main():
         except Exception:
             num_differences += 1
             if verbose:
-                var = colored(f'{v}', color='yellow', attrs=['bold'])
-                msg = (colored('NOVAR: ', color='yellow', attrs=['bold']) +
-                       f'Variable {var} found in {output1} but not {output2}')
+                var = colored(f"{v}", color="yellow", attrs=["bold"])
+                msg = (colored("NOVAR: ", color="yellow", attrs=["bold"]) +
+                       f"Variable {var} found in {output1} but not {output2}")
                 print(msg)
             continue
 
@@ -214,26 +214,26 @@ def main():
         if maxdiff is None:
             num_differences += 1
             if verbose:
-                var = colored(f'{v}', color='red', attrs=['bold'])
-                msg = (colored('ERROR: ', color='red', attrs=['bold']) +
-                       f'Variable {var} has inconsistent shapes: ' +
-                       f'{output1} = {v1.shape} and ' +
-                       f'{output2} = {v2.shape}')
+                var = colored(f"{v}", color="red", attrs=["bold"])
+                msg = (colored("ERROR: ", color="red", attrs=["bold"]) +
+                       f"Variable {var} has inconsistent shapes: " +
+                       f"{output1} = {v1.shape} and " +
+                       f"{output2} = {v2.shape}")
                 print(msg)
         elif not same:
             num_differences += 1
             if verbose:
-                var = colored(f'{v}', color='red', attrs=['bold'])
-                msg = (colored('DIFF:  ', color='red', attrs=['bold']) +
-                       f'Variable {var} has differences, max difference: ' +
-                       f'{maxdiff}')
+                var = colored(f"{v}", color="red", attrs=["bold"])
+                msg = (colored("DIFF:  ", color="red", attrs=["bold"]) +
+                       f"Variable {var} has differences, max difference: " +
+                       f"{maxdiff}")
                 print(msg)
         else:
             if verbose == 2:
-                msg = ''
-                var = colored(f'{v}', color='blue', attrs=['bold'])
-                msg = (colored('PASS:  ', color='blue', attrs=['bold']) +
-                       f'Variable {var} is the same in both outputs')
+                msg = ""
+                var = colored(f"{v}", color="blue", attrs=["bold"])
+                msg = (colored("PASS:  ", color="blue", attrs=["bold"]) +
+                       f"Variable {var} is the same in both outputs")
                 print(msg)
 
     # Close the ADIOS2 output
@@ -242,8 +242,8 @@ def main():
 
     # Final summary of differences
     if num_differences == 0:
-        print(colored(f'{output1} and {output2} are identical', attrs=['bold']))
+        print(colored(f"{output1} and {output2} are identical", attrs=["bold"]))
     else:
-        print(colored(f'{num_differences} differences found between {output1} and {output2}', attrs=['bold']))
+        print(colored(f"{num_differences} differences found between {output1} and {output2}", attrs=["bold"]))
 
     sys.exit(0 if num_differences == 0 else 1)
